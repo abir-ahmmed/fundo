7e571aab6d1d0144d131c2c9b9c22d2be4acd968
Abir Ahmmed <abirahmmedrana@gmail.com>
2023-01-14 22:53:54 +0600

    manifest

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        gallery: {
+          enabled: true
+        }
+    });
+    
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+    });
+    
